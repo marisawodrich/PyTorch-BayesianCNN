429a5af61cba97e43e5fab2d43bf38583039bd5e
marisawodrich <marisa.wodrich@gmail.com>
2023-10-17 17:43:16 +0200

    new models

--- config_bayesian.py
+++ config_bayesian.py
@@ -8,11 +8,11 @@ priors={
     'posterior_rho_initial': (-5, 0.1),  # (mean, std) normal_
 }
 
-n_epochs = 30
-lr_start = 0.0005
+n_epochs = 25
+lr_start = 0.0001
 num_workers = 4
 valid_size = 0.0
 batch_size = 32
-train_ens = 15
-valid_ens = 5
+train_ens = 10
+valid_ens = 1
 beta_type = 0.1  # 'Blundell', 'Standard', etc. Use float for const value
