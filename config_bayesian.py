7f68ec9994e214b5dbbfaf9b8c77c057ef97823f
marisawodrich <marisa.wodrich@gmail.com>
2023-10-26 13:45:27 +0200

    fixed OOD and UQ errors

--- config_bayesian.py
+++ config_bayesian.py
@@ -2,10 +2,10 @@
 layer_type = 'bbb'  # 'bbb' or 'lrt'
 activation_type = 'relu'  # 'softplus' or 'relu'
 priors={
-    'prior_mu': 1,
-    'prior_sigma': 0.5,
-    'posterior_mu_initial': (1, 0.5),  # (mean, std) normal_
-    'posterior_rho_initial': (-5, 0.5),  # (mean, std) normal_
+    'prior_mu': 0,
+    'prior_sigma': 0.1,
+    'posterior_mu_initial': (0, 0.1),  # (mean, std) normal_
+    'posterior_rho_initial': (-5, 0.1),  # (mean, std) normal_
 }
 
 n_epochs = 40
